91ae5f39c3c0569f27ca0f34ec7a3b823524bc47
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-19 04:00:07 +0200

    make_fix_root_file

--- R/file.R
+++ R/file.R
@@ -9,7 +9,10 @@
 #' \code{\link[base]{file.path}}.
 #' The function \code{make_find_root_file} can be used to create shorthand functions
 #' such as \code{find_rstudio_root_file} and \code{find_package_root_file}.
-#' You may want to alias the latter two with a much shorter name in your code.
+#' In many cases, the function \code{make_fix_root_file} is even more useful:
+#' It creates a shorthand function that is tied to a fixed root (as opposed to
+#' \code{make_find_root_file} which creates a function that will look for the
+#' root every time it is called).
 #'
 #' @param filename,contents Regular expressions to match the file name or contents
 #' @param path The start directory
@@ -21,6 +24,7 @@
 #' @examples
 #' find_package_root_file("tests", "testthat.R")
 #' make_find_root_file(glob2rx("DESCRIPTION"), "^Package: ")
+#' make_fix_root_file(glob2rx("DESCRIPTION"), "^Package: ")
 #'
 #' @seealso \code{\link{find_root}} \code{\link[utils]{glob2rx}} \code{\link[base]{file.path}}
 #'
--- R/rrmake.R
+++ R/rrmake.R
@@ -11,6 +11,15 @@ make_find_root_file <- function(filename, contents = NULL, n = -1L) {
 
 #' @rdname find_root_file
 #' @export
+make_fix_root_file <- function(filename, contents = NULL, n = -1L) {
+  root <- find_root(filename = filename, contents = contents, n = n)
+  eval(bquote(function(...) {
+    file.path(.(root), ...)
+  }))
+}
+
+#' @rdname find_root_file
+#' @export
 find_rstudio_root_file <- make_find_root_file(glob2rx("*.Rproj"), contents = "^Version: ", n = 1L)
 
 #' @rdname find_root_file
