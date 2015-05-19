56dcb23b9f2fb0250813bdfd0953dfe33c6bcd9e
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-19 02:25:01 +0200

    rename functions

--- R/rrmake.R
+++ R/rrmake.R
@@ -1,17 +1,17 @@
 #' @rdname find_root_file
-make_find_root <- function(filename, contents = NULL, n = -1L) {
+make_find_root_file <- function(filename, contents = NULL, n = -1L) {
   force(filename)
   force(contents)
   force(n)
-  ret <- function(path = getwd()) {
-    find_root(filename = filename, contents = contents, n = n, path = path)
+  ret <- function(..., path = getwd()) {
+    find_root_file(filename = filename, contents = contents, n = n, ..., path = path)
   }
   environment(ret) <- environment(find_root)
   ret
 }
 
 #' @rdname find_root_file
-find_rstudio_root <- make_find_root(glob2rx("*.Rproj"), contents = "^Version: ", n = 1L)
+find_rstudio_root_file <- make_find_root_file(glob2rx("*.Rproj"), contents = "^Version: ", n = 1L)
 
 #' @rdname find_root_file
-find_package_root <- make_find_root(glob2rx("DESCRIPTION"), contents = "^Package: ", n = 1L)
+find_package_root_file <- make_find_root_file(glob2rx("DESCRIPTION"), contents = "^Package: ", n = 1L)
