78f64e34ecd8143b5f6cdbf272f9d8b5007ce03c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-20 14:34:16 +0200

    fix

--- R/rrmake.R
+++ R/rrmake.R
@@ -18,8 +18,8 @@ make_fix_root_file <- function(criterion) {
 
 #' @rdname find_root_file
 #' @export
-find_rstudio_root_file <- make_find_root_file(has_file(glob2rx("*.Rproj"), contents = "^Version: ", n = 1L))
+find_rstudio_root_file <- make_find_root_file(has_file_pattern(glob2rx("*.Rproj"), contents = "^Version: ", n = 1L))
 
 #' @rdname find_root_file
 #' @export
-find_package_root_file <- make_find_root_file(has_file(glob2rx("DESCRIPTION"), contents = "^Package: ", n = 1L))
+find_package_root_file <- make_find_root_file(has_file("DESCRIPTION", contents = "^Package: ", n = 1L))
