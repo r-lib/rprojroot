c0f3c3833ea76777d420ec2c211dd3084338a592
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-20 10:59:46 +0200

    docs

--- R/criterion.R
+++ R/criterion.R
@@ -1,6 +1,12 @@
 #' Is a directory the project root?
 #'
-#' TBD
+#' Objects of the \code{root_criterion} class decide if a
+#' given directory is a project root.
+#'
+#' Construct criteria using \code{root_criterion} in a very general fashion
+#' by specifying a function with a \code{path} argument, and a description.
+#' The \code{has_file} function constructs a criterion that checks for the
+#' existence of a specific file with specific contents.
 #'
 #' @param testfun A function with one parameter that returns \code{TRUE}
 #'   if the directory specified by this parameter is the project root,
