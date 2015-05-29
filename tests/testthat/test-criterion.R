f20e9a24a7688e997e09cbcd1dfaa6e9af46e2b4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-29 20:34:31 +0200

    add explicit test for as.root_criterion.character

--- tests/testthat/test-criterion.R
+++ tests/testthat/test-criterion.R
@@ -4,4 +4,5 @@ test_that("is.root_criterion", {
   expect_true(is.root_criterion(has_file("DESCRIPTION")))
   expect_false(is.root_criterion("DESCRIPTION"))
   expect_true(is.root_criterion(as.root_criterion("DESCRIPTION")))
+  expect_equal(as.root_criterion("x"), has_file("x"))
 })
