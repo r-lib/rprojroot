f5ce2dfb1ccccb34d3dc6a50d9927b947eeeccdc
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-11 21:45:21 +0200

    remove references to removed make_fix_root_file()

--- R/file.R
+++ R/file.R
@@ -7,12 +7,6 @@
 #' \code{\link{find_root}} that
 #' appends an arbitrary number of path components to the root using
 #' \code{\link[base]{file.path}}.
-#' The function \code{make_find_root_file} can be used to create shorthand functions
-#' such as \code{find_rstudio_root_file} and \code{find_package_root_file}.
-#' In many cases, the function \code{make_fix_root_file} is even more useful:
-#' It creates a shorthand function that is tied to a fixed root (as opposed to
-#' \code{make_find_root_file} which creates a function that will look for the
-#' root every time it is called).
 #'
 #' @param criterion A criterion, will be coerced using
 #'   \code{\link{as.root_criterion}}
@@ -24,8 +18,8 @@
 #' @examples
 #' \dontrun{
 #' find_package_root_file("tests", "testthat.R")
-#' make_find_root_file(glob2rx("DESCRIPTION"), "^Package: ")
-#' make_fix_root_file(glob2rx("DESCRIPTION"), "^Package: ")
+#' has_file("DESCRIPTION", "^Package: ")$find_file
+#' has_file("DESCRIPTION", "^Package: ")$make_fix_file(".")
 #' }
 #'
 #' @seealso \code{\link{find_root}} \code{\link[utils]{glob2rx}} \code{\link[base]{file.path}}
