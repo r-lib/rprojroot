766b5cef55dbdf041edad3eb2580907c6c09a2a9
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-23 23:06:33 +0200

    new find_testthat_root_file
    `find_testthat_root_file`

--- R/shortcut.R
+++ R/shortcut.R
@@ -9,3 +9,7 @@ find_package_root_file <- is_r_package$find_file
 #' @rdname find_root_file
 #' @export
 find_remake_root_file <- is_remake_project$find_file
+
+#' @rdname find_root_file
+#' @export
+find_testthat_root_file <- is_testthat$find_file
