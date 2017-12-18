329bc17a2e69f1ed88a1176f30735a5e2ee6e346
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-18 16:37:32 +0100

    try revdep checks

--- tic.R
+++ tic.R
@@ -1,18 +1,33 @@
-add_package_checks()
+if (ci()$get_branch() != "revdep") {
+  add_package_checks()
 
-if (inherits(ci(), "TravisCI") && Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
+  if (inherits(ci(), "TravisCI") && Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
+    # pkgdown documentation can be built optionally. Other example criteria:
+    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+    # - `ci()$is_tag()`: Only for tags, not for branches
+    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh())
 
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown(examples = FALSE)) %>%
-    add_step(step_push_deploy(
-      path = "docs",
-      branch = if (ci()$get_branch() == "master") "gh-pages" else paste0("gh-pages-", ci()$get_branch())
-    ))
+    get_stage("deploy") %>%
+      add_step(step_build_pkgdown(examples = FALSE)) %>%
+      add_step(step_push_deploy(
+        path = "docs",
+        branch = if (ci()$get_branch() == "master") "gh-pages" else paste0("gh-pages-", ci()$get_branch())
+      ))
+  }
+} else {
+  get_stage("script") %>%
+    add_code_step(
+      {
+        revdepcheck::revdep_check(dependencies = c()) # initialize
+        revdepcheck::revdep_add(packages = strsplit(Sys.getenv("REVDEP_PKG"), ", *")[[1]])
+        revdepcheck::revdep_check(num_workers = 2)
+        revdepcheck::revdep_report_problems()
+      },
+      {
+        remotes::install_github("r-lib/revdepcheck")
+      }
+    )
 }
