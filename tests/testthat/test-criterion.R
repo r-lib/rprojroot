f5b267ab4543b4c0d2e6ba7cf2b8803932ad2286
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-10 07:49:52 +0100

    Move test

--- tests/testthat/test-criterion.R
+++ tests/testthat/test-criterion.R
@@ -1,3 +1,25 @@
+test_that("Shortcuts", {
+  expect_equal(
+    make_find_root_file("testthat.R")("testthat"),
+    normalizePath(getwd(), winslash = "/")
+  )
+
+  R <- make_fix_root_file("testthat.R", getwd())
+
+  oldwd <- withr::local_dir("~")
+
+  expect_equal(
+    normalizePath(R("testthat"), mustWork = TRUE),
+    normalizePath(oldwd, mustWork = TRUE)
+  )
+
+  path <- R()
+  expect_equal(
+    normalizePath(R(path, "testthat"), mustWork = TRUE),
+    normalizePath(oldwd, mustWork = TRUE)
+  )
+})
+
 test_that("root_criterion", {
   expect_error(
     suppressWarnings(root_criterion(5, "Bogus")),
